60f8f4d84338d091ad80f1cba4d20201229ff491
mmiah00 <mmiah00@stuy.edu>
2020-01-31 11:56:06 -0500

    changed to python file

---
+++ picmaker.py
@@ -0,0 +1,24 @@
+import random
+f = open ("pic.ppm", "w")
+
+f.write ("P3 500 500 255")
+
+color = [][]
+
+def placecolors (size):
+    red, green, blue
+    for i in range (500):
+        for j in range (500):
+            if j < 500/3:
+                red = 255
+                green = Random.randint (256)
+                blue = Random.randint (256)
+            elif j < (500/3)*2:
+                red = Random.randint (256)
+                green = 255
+                blue = Random.randint (256)
+            else:
+                red = Random.randint (256)
+                green = Random.randint (256)
+                blue = 255
+        color [i][0] = 
