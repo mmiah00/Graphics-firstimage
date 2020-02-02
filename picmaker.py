fa8a538536340712a3517fcb422d94af6e77fc7d
mmiah00 <mmiah00@stuy.edu>
2020-02-01 23:58:32 -0500

    hyberbola

--- picmaker.py
+++ picmaker.py
@@ -17,7 +17,7 @@ colors = []
 def placecolors ():
     for i in range (500):
         for j in range (500):
-            if pow (j - 250, 2) + pow (i - 250, 2) > 2500:
+            if pow (i - 250, 2) - pow (j - 250, 2) > 2500:
                 f.write ("0 255 0 ")
             else:
                 f.write ("0 0 0 ")
