a029c14353ee24cfc7704b86f0ab4caf08e3b150
mmiah00 <mmiah00@stuy.edu>
2020-02-02 04:22:42 +0000

    should make a red circle w black background

--- picmaker.py
+++ picmaker.py
@@ -15,8 +15,12 @@ color = []
 colors = []
 
 def placecolors ():
-    for i in range (500*500):
-        f.write ("255 0 0 ")
+    for i in range (500):
+        for j in range (500):
+            if pow (i - 250, 2) + pow (j - 250, 2) == 2500:
+                f.write ("255 0 0 ")
+            else:
+                f.write ("0 0 0 ")
 
 # def toFile ():
 #     for i in range (len (colors)):
