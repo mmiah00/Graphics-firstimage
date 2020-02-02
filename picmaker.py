675a61190c95d0b114bd5b24b897d34fe6f34b00
mmiah00 <mmiah00@stuy.edu>
2020-02-02 03:29:45 +0000

    testing

--- picmaker.py
+++ picmaker.py
@@ -5,24 +5,31 @@ f.write ("P3 500 500 255 ")
 
 color = []
 
-def placecolors ():
-    red, green, blue = 0,0,0
-    for i in range (500):
-        for j in range (500):
-            if j < 500/3:
-                red = 255
-                green = random.randint (0,256)
-                blue = random.randint (0,256)
-            elif j < (500/3)*2:
-                red = random.randint (0,256)
-                green = 255
-                blue = random.randint (0,256)
-            else:
-                red = random.randint (0,256)
-                green = random.randint (0,256)
-                blue = 255
-        colors = [red, green, blue]
-        color.append (colors)
-        f.write (str (red) + " " + str (green) + " " + str (blue) + " ")
+# def placecolors ():
+#     red, green, blue = 0,0,0
+#     for i in range (500):
+#         for j in range (500):
+#             if j < 500/3:
+#                 red = 255
+#                 green = random.randint (0,256)
+#                 blue = random.randint (0,256)
+#             elif j < (500/3)*2:
+#                 red = random.randint (0,256)
+#                 green = 255
+#                 blue = random.randint (0,256)
+#             else:
+#                 red = random.randint (0,256)
+#                 green = random.randint (0,256)
+#                 blue = 255
+#         colors = [red, green, blue]
+#         color.append (colors)
+#         f.write (str (red) + " " + str (green) + " " + str (blue) + " ")
 
+def placecolors ():
+    for i in range (500*500):
+        red = rand.randint (1,255)
+        green = rand.randint (1,255)
+        blue = rand.randint (1,255)
+        f.write (str (red) + " " + str (green) + " " + str (blue) + " ")
+        
 placecolors ()
