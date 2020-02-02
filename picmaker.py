47883517f4e3f4abb995bc951f5affe2205f9f31
mmiah00 <mmiah00@stuy.edu>
2020-02-02 03:42:43 +0000

    idk what its gonna do lol

--- picmaker.py
+++ picmaker.py
@@ -6,30 +6,30 @@ f.write ("P3 500 500 255 ")
 color = []
 
 # def placecolors ():
-#     red, green, blue = 0,0,0
-#     for i in range (500):
-#         for j in range (500):
-#             if j < 500/3:
-#                 red = 255
-#                 green = random.randint (0,256)
-#                 blue = random.randint (0,256)
-#             elif j < (500/3)*2:
-#                 red = random.randint (0,256)
-#                 green = 255
-#                 blue = random.randint (0,256)
-#             else:
-#                 red = random.randint (0,256)
-#                 green = random.randint (0,256)
-#                 blue = 255
-#         colors = [red, green, blue]
-#         color.append (colors)
+#     for i in range (500*500):
+#         red = random.randint (1,255)
+#         green = random.randint (1,255)
+#         blue = random.randint (1,255)
 #         f.write (str (red) + " " + str (green) + " " + str (blue) + " ")
 
+colors = []
+
 def placecolors ():
-    for i in range (500*500):
-        red = random.randint (1,255)
-        green = random.randint (1,255)
-        blue = random.randint (1,255)
-        f.write (str (red) + " " + str (green) + " " + str (blue) + " ")
+    for y in range (500):
+        for x in range (500):
+            if pow (y + 250, 2) - pow (x + 250, 2) == 1:
+                color = [255, 100, 34]
+                colors.append (color)
+            elif y % 4 == 0 and x % 4 == 0:
+                color = [0, 0, 255]
+                colors.append (color)
+            else:
+                color = [100, 250, 90]
+                colors.append (color)
+
+def toFile ():
+    for i in range (len (colors)):
+        f.write (str (colors[i][0]) + " " + str (colors[i][1]) + str (colors[i][2]) + " ")
 
 placecolors ()
+toFile ()
