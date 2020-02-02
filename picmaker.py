03e9c7bbb3c059efdac932ba9b9c442574b9d514
mmiah00 <mmiah00@stuy.edu>
2020-02-02 02:54:04 +0000

    compiles

--- picmaker.py
+++ picmaker.py
@@ -3,27 +3,26 @@ f = open ("pic.ppm", "w")
 
 f.write ("P3 500 500 255")
 
-color = [][]
+color = []
 
-def placecolors (size):
-    red, green, blue
+def placecolors ():
+    red, green, blue = 0,0,0
     for i in range (500):
         for j in range (500):
             if j < 500/3:
                 red = 255
-                green = Random.randint (256)
-                blue = Random.randint (256)
+                green = random.randint (0,256)
+                blue = random.randint (0,256)
             elif j < (500/3)*2:
-                red = Random.randint (256)
+                red = random.randint (0,256)
                 green = 255
-                blue = Random.randint (256)
+                blue = random.randint (0,256)
             else:
-                red = Random.randint (256)
-                green = Random.randint (256)
+                red = random.randint (0,256)
+                green = random.randint (0,256)
                 blue = 255
-        color [i][0] = red
-        color [i][1] = green
-        color [i][2] = blue
-        f.write (red + " " + green + " " + blue)
+        colors = [red, green, blue]
+        color.append (colors)
+        f.write (str (red) + " " + str (green) + " " + str (blue) + " ")
 
-f = open ("pic.ppm", "v")
+placecolors ()
