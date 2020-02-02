d6f4042bced538b0ce5522a079294104604c055a
mmiah00 <mmiah00@stuy.edu>
2020-02-02 05:17:06 +0000

    clearned it up

--- picmaker.py
+++ picmaker.py
@@ -3,15 +3,6 @@ f = open ("pic.ppm", "w")
 
 f.write ("P3 500 500 255 ")
 
-color = []
-
-# def placecolors ():
-#     for i in range (500*500):
-#         red = random.randint (1,255)
-#         green = random.randint (1,255)
-#         blue = random.randint (1,255)
-#         f.write (str (red) + " " + str (green) + " " + str (blue) + " ")
-
 colors = []
 
 def placecolors ():
@@ -22,9 +13,5 @@ def placecolors ():
             else:
                 f.write ("0 0 0 ")
 
-# def toFile ():
-#     for i in range (len (colors)):
-#         f.write (str (colors[i][0]) + " " + str (colors[i][1]) + str (colors[i][2]) + " ")
-
 placecolors ()
-# toFile ()
+f.close ()
